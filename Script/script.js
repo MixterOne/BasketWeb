3f16edf426458640e33a7f7e8c7afa565f5f909d
MixterOne <matheusisaias879@gmail.com>
2022-01-07 18:38:38 -0300

    confirmar

--- Script/script.js
+++ Script/script.js
@@ -58,4 +58,16 @@ function curti3(){
     
 }
 
+function mudaBackgMJ(){
+    let container = document.querySelector("#jogadores")
+    container.style.background = "url(../Img/nba-jordan.gif) no-repeat"
+    container.style.backgroundSize = "cover"
+
+}
+
+function mudaBackgLB(){
+    let container = document.querySelector("#jogadores")
+    container.style.background = "url(../Img/LeMJ.gif) no-repeat";
+    container.style.backgroundSize = "cover";
+}
 
